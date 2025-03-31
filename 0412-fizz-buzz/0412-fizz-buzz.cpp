a7a9a8fecee5d8ddaeb053fcb001a4f1e89bbee8
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2025-03-31 19:24:36 +0530

    Time: 1 ms (53.37%), Space: 11.6 MB (77.83%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        int fizz = 0,buzz=0;
+        for(int i = 1;i<=n;i++){
+            fizz++,buzz++;
+            if(fizz == 3 && buzz == 5){
+                result.push_back("FizzBuzz");
+                fizz=0,buzz=0;
+            }
+            else if(fizz == 3){
+                result.push_back("Fizz");
+                fizz=0;
+            }
+            else if(buzz == 5){
+                result.push_back("Buzz");
+                buzz=0;
+            }
+            else{
+                result.push_back(to_string(i));
+            }
+        }
+        return result;
+    }
+};
