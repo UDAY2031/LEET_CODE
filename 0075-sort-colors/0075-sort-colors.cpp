9fd1752e4f848538bc4ecb19c64c5605067977b6
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2025-09-03 22:07:18 +0530

    Time: 0 ms (100%), Space: 11.8 MB (12.17%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
+        vector<int> freq(3,0);
 
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n-i-1; j++){
-                if(nums[j] > nums[j+1]){
-                    swap(nums[j],nums[j+1]);
-                }
+            freq[nums[i]]++;
+        }
+
+        int j = 0;
+        for(int i = 0; i < 3; i++){
+            while(freq[i] > 0){
+                nums[j++] = i;
+                freq[i]--;
             }
         }
     }
