8eb70f3295adf1305a673915fc9b4f0069b1ecd4
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2025-09-08 13:12:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxi = 0;
+        for(int i = 0; i < n; i++){
+            int mini = prices[i];
+            int profit = 0;
+            if(prices[i+1] > mini){
+                profit = prices[i+1] - mini;
+            }
+            maxi += profit;
+        }
+        return maxi;
+    }
+};
