0a0d44ef41a8c18aff34b147bb86b68c1fcdae20
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2025-09-16 22:12:59 +0530

    Time: 0 ms (100%), Space: 26 MB (91.83%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,17 +2,11 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        vector<int> hash(100001,0);
+        int ans = 0;
 
         for(int i = 0; i < n; i++){
-            hash[nums[i]]++;
+            ans ^= nums[i];
         }
-
-        for(int i = 0; i < hash.size(); i++){
-            if(hash[i] == 1){
-                return i;
-            }
-        }
-        return -1;
+        return ans;
     }
 };
