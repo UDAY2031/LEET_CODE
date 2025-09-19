edb68a40b219e1663390d79ba444604e9035805d
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2025-09-19 17:19:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n <= 0) return false;
-        while(n % 4 == 0) n /= 4;
-        return n == 1;
+        if(n > 0 && n == 1) return true;
+        if((n & (n - 1)) == 0) return true;
+        return false;
     }
 };
