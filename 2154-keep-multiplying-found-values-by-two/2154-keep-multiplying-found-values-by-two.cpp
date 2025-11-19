ac143efb58b18d9a7faaf5f4b4c0efe192ccb420
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2025-11-19 20:50:24 +0530

    Time: 8 ms (7.87%), Space: 15.8 MB (20%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -4,10 +4,8 @@ public:
         int n = nums.size();
         set<int> st(nums.begin(), nums.end());
 
-        for(int i = 0; i < st.size(); i++){
-            if(st.find(original) != st.end()){
-                original = original * 2;
-            }
+        while(st.find(original) != st.end()){
+            original *= 2;
         }
         return original;
     }
